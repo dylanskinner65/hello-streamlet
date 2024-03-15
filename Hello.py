da4d4101f2589a505ad94b621076590ae4118950
Dylan Skinner <98558549+dylanskinner65@users.noreply.github.com>
2024-03-15 22:11:31 +0000

    Pushing changes. Still running into libGL.so.1 problems..

--- Hello.py
+++ Hello.py
@@ -14,37 +14,53 @@
 
 import streamlit as st
 from streamlit.logger import get_logger
+from PIL import Image
+import numpy as np
+from ultralytics import YOLO
 
 LOGGER = get_logger(__name__)
 
+# Function to perform object detection with YOLO model
+def detect_objects(image, column):
+    # Your code for object detection with YOLOv8 goes here
+    # Load in the model
+    model = YOLO("best.pt")
+
+    # Perform object detection
+    prediction = model.predict(image)
+
+    # Visualize the results
+    for i, r in enumerate(prediction):
+        # Plot results image
+        im_bgr = r.plot()  # BGR-order numpy array
+        im_rgb = Image.fromarray(im_bgr[..., ::-1])  # RGB-order PIL image
+
+    # Show predicted image next to the input image
+    column.image(im_rgb, caption=f"Predicted Image", use_column_width=True)
 
 def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
 
-    st.write("# Welcome to Streamlit! 👋")
+    # st.write("# YOLO Welcome to Streamlit! 👋")
+    st.markdown("<h1 style='text-align: center;'>Trash Detector</h1>", unsafe_allow_html=True)
+    
+    # Upload image
+    uploaded_image = st.file_uploader("Please upload an image", type=["jpg", "jpeg", "png"])
+    
+    # Set up columns for predicted and original images
+    col1, col2 = st.columns(2)
 
-    st.sidebar.success("Select a demo above.")
+    if uploaded_image is not None:
+        # Display uploaded image
+        image = Image.open(uploaded_image)
+        col1.image(image, caption="Uploaded Image", use_column_width=True)
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+        # Perform object detection when button is clicked
+        if st.button("Detect Objects"):
+            # Convert image to numpy array
+            image_np = np.array(image)
+
+            # Perform object detection
+            detect_objects(image_np, col2)
 
 
 if __name__ == "__main__":
